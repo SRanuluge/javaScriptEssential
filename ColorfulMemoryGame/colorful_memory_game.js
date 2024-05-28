942cb7368600482444b8e24cb5b662fa3b83dcad
SRanuluge <sranuluge.dev@gmail.com>
2024-05-28 16:47:38 +1200

    add a game start function

--- ColorfulMemoryGame/colorful_memory_game.js
+++ ColorfulMemoryGame/colorful_memory_game.js
@@ -1,21 +1,34 @@
-const colors = ['red', 'blue', 'green', 'purple', 'orange', 'pink', 'red','blue','green','purple', 'orange','pink']
-let cards = shuffle(colors.concat(colors))
-let selectedcards = []
-let  score = 0
-let timeLeft = 30
+const colors = [
+  "red",
+  "blue",
+  "green",
+  "purple",
+  "orange",
+  "pink",
+  "red",
+  "blue",
+  "green",
+  "purple",
+  "orange",
+  "pink",
+];
+let cards = shuffle(colors.concat(colors));
+let selectedcards = [];
+let score = 0;
+let timeLeft = 30;
 let gameInterval;
 
-const startbtn = document.getElementById('startbtn')
-const gameContainer = document.getElementById('game-conatainer')
-const scoreElement = document.getElementById('score') 
-const timerElement = document.getElementById('timer')
+const startbtn = document.getElementById("startbtn");
+const gameContainer = document.getElementById("game-conatainer");
+const scoreElement = document.getElementById("score");
+const timerElement = document.getElementById("timer");
 
 function generateCards() {
-    for(const color of cards) {
-        const card = document.createElement('div')
-        card.classList.add('card')
-        card.dataset.color = color
-        card.textContent = '?'
-        gameContainer.appendChild(card)
+  for (const color of cards) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.dataset.color = color;
+    card.textContent = "?";
+    gameContainer.appendChild(card);
+  }
 }
-    }
